47e3ba27772b763093fab54c366a36f76419600d
bug5654 <bug.new@gmail.com>
2025-04-28 08:24:08 -0700

    corrected typo, added debug info, added await to not be RAM limited

--- allassert.js
+++ allassert.js
@@ -1,8 +1,12 @@
-//shouldn't work?  added to buyserver so not as much and issue
+var hackedServer = "max-hardware";
+var script = "hack.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
+  var debug;
   for (var host of ns.getPurchasedServers()) {
-    await ns.exec("assertinstructions.js","home",1,host);
+    ns.tprint("asserting on ",host);
+    debug = await ns.exec("assertinstruction.js","home",1,host,hackedServer,script); //await allows 1 execution at a time, not limited by RAM
+    ns.tprint(debug);
   }
 }
