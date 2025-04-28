3e9bc3c7780d8296437f60f0358a5232b6d45a89
bug5654 <bug.new@gmail.com>
2025-04-28 08:31:49 -0700

    grow first for en-masse execution

--- hack.js
+++ hack.js
@@ -3,9 +3,9 @@
 export async function main(ns) {
   var hackedserver = ns.args[0];
   while(true) {
-    await ns.hack(hackedserver);
-    await ns.weaken(hackedserver);
     await ns.grow(hackedserver);
+    await ns.weaken(hackedserver);
+    await ns.hack(hackedserver);
     await ns.weaken(hackedserver);
   }
 }
