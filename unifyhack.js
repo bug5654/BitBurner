24b730c1f76efd76aabd882ea6c2eb7ef339eddb
bug5654 <bug.new@gmail.com>
2025-05-01 11:28:13 -0700

    run the script on everything

---
+++ unifyhack.js
@@ -0,0 +1,64 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  var remoteScriptName = "assertinstruction.js";
+  //var targetServer = "foodnstuff"; //defined in assertinstructions
+
+  if(ns.args.length > 0) {
+    remoteScriptName = ns.args[0];
+    if(!( remoteScriptName.includes(".js"))) {
+      remoteScriptName = remoteScriptName + ".js"
+    }
+  }
+
+  var servers = getAllServers(ns); //recursively build server list
+  //ns.tprint(servers);  //DEBUG ONLY, COMMENT OUT IN PROD
+  for(var server of servers) {
+    ns.tprint("Running " + remoteScriptName + " " + server);
+    await ns.exec(remoteScriptName,"home",1,server);
+  }
+}
+
+function getAllServers(ns) {
+    const knownServers = new Set();
+    const newServers = new Set();
+    newServers.add("home");
+
+    while (newServers.size > 0) {
+        const server = newServers.values().next().value;
+        newServers.delete(server);
+        const neighbors = ns.scan(server);
+        for (const neighbor of neighbors) {
+            if (!knownServers.has(neighbor)) {
+                knownServers.add(neighbor);
+                newServers.add(neighbor);
+            }
+        }
+    }
+    return knownServers;
+    //ns.tprint([...knownServers]);
+}
+
+
+
+/*function getAllServers(_ns) {
+  var seenList = []; //init blank list
+  ScanServer("home", _ns, seenList); //recursively build server list tree from home
+  return seenList;
+}
+
+function ScanServer(serverName, _ns , seenList) {
+  //already in list?  Don't add
+  _ns.tprint("Server: "+ serverName);
+  _ns.tprint("seenList: " + seenList);
+	if (seenList.includes(serverName)) { return; }
+  //else
+  
+  //in list?  add and check for children
+  var connectedservers = _ns.scan(serverName);
+	seenList.push(serverName);
+
+  //check each child server
+  for(var node of connectedservers) {
+    ScanServer(node);
+  }
+}*/
