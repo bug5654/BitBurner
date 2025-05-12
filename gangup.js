7b26f17a1e95ff66e0e28b9b5ec44861a3e549ab
bug5654 <bug.new@gmail.com>
2025-05-12 07:57:00 -0700

    added and tested augment exclusion

--- gangup.js
+++ gangup.js
@@ -5,16 +5,44 @@ export async function main(ns) {
   var output = "";
   ns.tprint("Names:\n",names);
   ns.tprint("\n\nUpgrades:\n",upgrades);
-  var augments = ["Bionic","Brachi",""]
+  var augments = ["Bionic","Brachi","Nanofiber Weave","Synthetic Heart", "Synfibril","BitWire",
+    "Neuralstimulator","DataJack", "Graphene Bone Lacings"];  //purchased separately
+  var buyAugments = false;
+  if(ns.args.length > 0) {
+    if(ns.args[0] == "augment"){
+      buyAugments = true;
+      if(ns.args.length >1) {
+        names = ns.args;
+      }
+    }
+    else{
+      names = ns.args;
+    }
+  }
 
   for(var name of names) {
     for(var gear of upgrades) {
-      output = output + "\nattempting to buy " + gear + " for " + name + "\n";
-      output = output + ns.gang.purchaseEquipment(name, gear);
+      if((name != "augment") && (!(containedWithin(gear,augments))) || (buyAugments) ) {
+        output = output + "\nattempting to buy " + gear + " for " + name + "\n";
+        output = output + ns.gang.purchaseEquipment(name, gear);
+      }
+      if(containedWithin(gear,augments)) {
+         output = output + "\nrejected '" + gear + "' labeled as augment"
+      }
     }
     ns.tprint(output);  //debug
     output = "";  //debug
     return 0; //debug to ensure not spending all money on augments
   }
   ns.tprint(output);
+  
+}
+
+function containedWithin(src, partialMatches) {
+  for(var infix of partialMatches) {
+    if (src.includes(infix)) {  //partialMatches fuzzy matching src
+      return true;
+    }
+  }
+  return false;
 }
