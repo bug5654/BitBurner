6242d9b2d104f6153cdc0c8cb5fd34116c1b0186
bug5654 <bug.new@gmail.com>
2025-05-12 08:22:04 -0700

    select fire and augment working

--- gangup.js
+++ gangup.js
@@ -3,8 +3,8 @@ export async function main(ns) {
   var names = ns.gang.getMemberNames();
   var upgrades = ns.gang.getEquipmentNames();
   var output = "";
-  ns.tprint("Names:\n",names);
-  ns.tprint("\n\nUpgrades:\n",upgrades);
+  //ns.tprint("Names:\n",names);
+  //ns.tprint("\n\nUpgrades:\n",upgrades);
   var augments = ["Bionic","Brachi","Nanofiber Weave","Synthetic Heart", "Synfibril","BitWire",
     "Neuralstimulator","DataJack", "Graphene Bone Lacings"];  //purchased separately
   var buyAugments = false;
@@ -20,20 +20,28 @@ export async function main(ns) {
     }
   }
 
+  //ns.tprint("Names:\n",names); //debug
+  var totalCost = 0.0;
   for(var name of names) {
     for(var gear of upgrades) {
-      if((name != "augment") && (!(containedWithin(gear,augments))) || (buyAugments) ) {
-        output = output + "\nattempting to buy " + gear + " for " + name + "\n";
-        output = output + ns.gang.purchaseEquipment(name, gear);
+      if(!(name == "augment") && ((!(containedWithin(gear,augments))) || (buyAugments)) ) {
+        //ns.tprint( "\nattempting to buy " + gear + " for " + name + "\n"); //debug
+        //output = output + ns.gang.purchaseEquipment(name, gear);
+        if(ns.gang.purchaseEquipment(name, gear)) {
+          output = output + "\n Bought for " + name + ": \t" + gear + 
+            "\tPrice: $" + ns.formatNumber(ns.gang.getEquipmentCost(gear),3);
+          totalCost = totalCost + ns.gang.getEquipmentCost(gear);
+        }
       }
       if(containedWithin(gear,augments)) {
-         output = output + "\nrejected '" + gear + "' labeled as augment"
+         //output = output + "\nrejected '" + gear + "' labeled as augment"
       }
     }
-    ns.tprint(output);  //debug
-    output = "";  //debug
-    return 0; //debug to ensure not spending all money on augments
+    //ns.tprint(output);  //debug
+    //output = "";  //debug
+    //return 0; //debug to ensure not spending all money on augments
   }
+  output = output + "\n\nTotal spent: $" + ns.formatNumber(totalCost,3);
   ns.tprint(output);
   
 }
