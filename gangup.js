5f1ac82839a40c4afabab43ee40d59dd61e1c7e7
bug5654 <bug.new@gmail.com>
2025-05-12 07:21:14 -0700

    in vs of operator

--- gangup.js
+++ gangup.js
@@ -3,11 +3,14 @@ export async function main(ns) {
   var names = ns.gang.getMemberNames();
   var upgrades = ns.gang.getEquipmentNames();
   var output = "";
-  for(var name in names) {
-    for(var gear in upgrades) {
+  ns.tprint("Names:\n",names);
+  ns.tprint("\n\nUpgrades:\n",upgrades);
+  for(var name of names) {
+    for(var gear of upgrades) {
       output = output + "\nattempting to buy " + gear + " for " + name + "\n";
       output = output + ns.gang.purchaseEquipment(name, gear);
     }
+    return 0; //debug to ensure not spending all money on augments
   }
   ns.tprint(output);
 }
