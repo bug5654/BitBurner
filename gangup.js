607a7397c811298d78e1b22f667a15e85e0c5285
bug5654 <bug.new@gmail.com>
2025-05-14 15:33:09 -0700

    functional, but overly verbose for prod script

--- gangup.js
+++ gangup.js
@@ -28,8 +28,8 @@ export async function main(ns) {
         //ns.tprint( "\nattempting to buy " + gear + " for " + name + "\n"); //debug
         //output = output + ns.gang.purchaseEquipment(name, gear);
         if(ns.gang.purchaseEquipment(name, gear)) {
-          output = output + "\n Bought for " + name + ": \t" + gear + 
-            "\tPrice: $" + ns.formatNumber(ns.gang.getEquipmentCost(gear),3);
+          output = output + "\n Bought for " + padTab(3,name + ":") + padTab(3,gear) + 
+            "Price: $" + ns.formatNumber(ns.gang.getEquipmentCost(gear),3);
           totalCost = totalCost + ns.gang.getEquipmentCost(gear);
         }
       }
@@ -42,6 +42,13 @@ export async function main(ns) {
     //return 0; //debug to ensure not spending all money on augments
   }
   output = output + "\n\nTotal spent: $" + ns.formatNumber(totalCost,3);
+  if(totalCost==0.0) {
+    output = output + "\nMay have been no ";
+    if(buyAugments == false) {
+      output = output + "non-augment ";
+    }
+    output = output + "upgrades to buy OR not enough $ to purchase any upgrades for " + names;
+  }
   ns.tprint(output);
   
 }
@@ -53,4 +60,15 @@ function containedWithin(src, partialMatches) {
     }
   }
   return false;
+}
+
+function padTab(totalTabs, input) {
+  var tabstaken = input.length/8; //length of a tab is 8 spaces
+  var tabsToInsert = totalTabs - tabstaken;
+
+  var output = input;
+  for(var i=0; i<tabsToInsert; i++)
+    output = output + "\t";
+
+  return output;
 }
